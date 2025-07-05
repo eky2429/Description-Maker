8425584a34be005572bdcc28835cb8a11ab36b67
eky2429 <eky2429@gmail.com>
2025-07-05 17:12:08 -0500

    Added possible tracks to add in description as well as other organizations to give credit to

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import os
 
 #Stores directory to look into
-directory_path = r"D:\Youtube.dra\MediaFiles\OG Library\Music"
+directory_path = r"C:\Users\eky38\Videos\Mikeeey Schoolboy runaway.dra\MediaFiles\OG Library"
+
 #Stores the separator to use
 separator = "\\"
 
+sites_to_credit = [
+    "vecteezy"
+]
+
+#Tracks that may or may not be in the video:
+possible_tracks = [
+    "Atlantis - AudioNautix",
+    "Accepting Defeat - Experia",
+    "Carless Whisper",
+    "Come Thru",
+    "Danganronpa Mauve Pre Trial",
+    "Drop - Anno Domini Beats",
+    "Exhilarate",
+    "Jeopardy Theme",
+    "Tech diff Music"
+]
+
+kevin_macleod_cred = """by Kevin MacLeod (incompetech.com)
+Licensed under Creative Commons: By Attribution 4.0 License
+http://creativecommons.org/licenses/by/4.0/"""
+
+clips = []
 music = []
 
 def getFileName(file_path : str) -> str:
@@ -24,10 +47,21 @@ def list_files_recursive(path='.'):
             list_files_recursive(full_path)
         #Checks if file is inside a "Music" directory
         elif "Music" in full_path:
-            if "Kevin Macleod" in full_path:
-                music.append(getFileName(full_path) + " - Kevin Macleod")
+
+            #If track exists in list fo expected tracks, skip
+            if getFileName(full_path) in possible_tracks:
+                continue
+            #f track comes from kevin Macleod folder, provide credit
+            elif "Kevin Macleod" in full_path:
+                music.append(getFileName(full_path) + " - Kevin Macleod\n" + kevin_macleod_cred)
+            #Otehrwise, just add it to list
             else:
                 music.append(getFileName(full_path))
+        #If video comes from CLIPS
+        elif "Clips" in full_path:
+            for site in sites_to_credit:
+                if site in full_path and not(site in clips):
+                    clips.append(site + " - " +  str(getFileName(full_path)))
         #Does nothing else if cases above aren't met
         else:
             pass
@@ -36,9 +70,25 @@ def list_files_recursive(path='.'):
 # directory_path = input("Type in file path: ")
 
 list_files_recursive(directory_path)
+
 #Sorts the final array alphabetically
 music = sorted(music)
 
-#Prints each one individually
-for track in music:
-    print(track)
+#Includes other credit to give (from CLIPS)
+print("These are some sites to give credit to")
+for clip in clips:
+    print("O " + clip)
+print()
+
+#Prints list of possible track used
+if possible_tracks.count != 0:
+    print("These are the possible tracks that MAY or may not not be in video:")
+    for track in possible_tracks:
+        print("O " +  track)
+    print()
+
+if music.count != 0:
+    print("These are the tracks that are DEFINITELY in the video:")
+    #Prints each one individually
+    for track in music:
+        print("O " + track)
